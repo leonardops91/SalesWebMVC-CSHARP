88d45652e7dbbf02e83f1b62681eafe5b2f8b191
Leonardo <leonardops91@gmail.com>
2020-07-06 23:08:44 -0300

    Other entities and second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Salary { get; set; }
+        public Department Dapartment { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double salary, Department dapartment)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Salary = salary;
+            Dapartment = dapartment;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
